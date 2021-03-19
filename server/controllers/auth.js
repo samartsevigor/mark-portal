19869e9e9d86d6f6e9a8461b655cc219ff43053d
Samartsev Igor <igorsamartsev@gmail.com>
2021-03-19 23:32:33 +0400

    signup server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,19 @@
+import User from '../models/user.js'
+
 export const signup = (req, res) => {
-  console.log(req.body)
-  res.json({status: 'ok'})
+  const {name, email, password} = req.body
+  User.findOne({email}).exec((error, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: 'Email уже существует'
+      })
+    }
+  })
+  const newUser = new User({name, email, password})
+  newUser.save((error, success) => {
+     if (error){
+       return res.status(400).json({error})
+     }
+     return res.json({message: 'Регистрация успешна'})
+  })
 }
